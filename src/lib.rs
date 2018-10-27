587716a528580b91de43b97b06a379683ced7e7f
John Newbery <john@johnnewbery.com>
2018-10-27 19:52:44 -0400

    Improve docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 extern crate base64;
 use std::ops::BitXor;
 
-// A wrapper for Vec<u8> so we can define our own traits.
+/// A wrapper for `Vec<u8>` so we can define our own trait implementations.
 #[derive(Debug)]
 pub struct Bytes(Vec<u8>);
 
 impl Bytes {
+    /// Return a string containing the bytes represented as base64.
     pub fn to_base64(&self) -> String {
-        //! Return a string containing the bytes represented as base64
         base64::encode(&self.0)
     }
 
+    /// Construct a new Bytes object from a hex string slice.
     pub fn from_hex(hex: &str) -> Bytes {
-        //! Construct a new Bytes object from a hex string slice
         let mut ret = Vec::new();
         for i in 0..(hex.len()/2) {
             let res = u8::from_str_radix(&hex[2*i .. 2*i+2], 16);
@@ -28,8 +28,8 @@ impl Bytes {
 impl BitXor for Bytes {
     type Output = Bytes;
 
+    /// xor each of the bytes. Both Bytes objects must be the same length.
     fn bitxor(self, rhs: Bytes) -> Bytes {
-        //! xor each of the bytes. Both Bytes objects must be the same length.
         let mut ret = Vec::with_capacity(self.0.len());
         for i in 0..self.0.len() {
             ret.push(self.0[i] ^ rhs.0[i]);
@@ -39,8 +39,8 @@ impl BitXor for Bytes {
 }
 
 impl PartialEq for Bytes {
+    /// Bytes objects are equal if all the bytes are equal.
     fn eq(&self, other: &Bytes) -> bool {
-        //! Bytes objects are equal if all the bytes are equal
         self.0 == other.0
     }
 }
